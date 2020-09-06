8b85f8678b977b486264e54bc88cf0bbd9ba9dcb
yujin0131 <lyujin0131@naver.com>
2020-09-06 20:17:45 +0900

    Add when not in the location

--- Airdnd_Back/src/main/java/com/airdnd/back/SearchController.java
+++ Airdnd_Back/src/main/java/com/airdnd/back/SearchController.java
@@ -47,7 +47,6 @@ public class SearchController {
 			@RequestParam(value="facilityList", defaultValue="0")String facilityList, @RequestParam(value="hostLangList", defaultValue="0") String hostLangList,
 			@RequestParam(value="page", defaultValue="0")int page) {
 
-
 		try {
 			location = URLDecoder.decode(location, "utf-8");
 			checkIn = URLDecoder.decode(checkIn, "utf-8");
@@ -62,227 +61,239 @@ public class SearchController {
 		}
 
 		JSONObject res = new JSONObject();
+		JSONObject empty = new JSONObject();
+		List empty_list = new ArrayList<Object>();
 
-		//search_list : 페이지별 숙소 리스트------------------
-		Map<Object, Object> param = new HashMap();
+		if(!(location.equals("서울") || location.equals("제주도") || location.equals("괌") || location.equals("하와이"))) {
+			res.put("homes", empty_list);
+			res.put("dataTotal", 0);
+			res.put("filterCondition", empty);
+			res.put("priceArray", empty_list);
+			res.put("averagePrice", 0);
+			res.put("recenthomes", empty_list);
+		}else {
 
-		param.put("location", location);
-		param.put("page", page);
-		param.put("guests", guests);
-		param.put("priceMin", priceMin);
-		param.put("priceMax", priceMax);
-		param.put("bedCount", bedCount);
-		param.put("bedroomCount", bedroomCount);
-		param.put("bathCount", bathCount);
-		param.put("neLat", neLat);
-		param.put("neLng", neLng);
-		param.put("swLat", swLat);
-		param.put("swLng", swLng);
+			//search_list : 페이지별 숙소 리스트------------------
+			Map<Object, Object> param = new HashMap();
 
-		//if(filter_roomType_house)
+			param.put("location", location);
+			param.put("page", page);
+			param.put("guests", guests);
+			param.put("priceMin", priceMin);
+			param.put("priceMax", priceMax);
+			param.put("bedCount", bedCount);
+			param.put("bedroomCount", bedroomCount);
+			param.put("bathCount", bathCount);
+			param.put("neLat", neLat);
+			param.put("neLng", neLng);
+			param.put("swLat", swLat);
+			param.put("swLng", swLng);
 
-		List<AirdndSearchVO> search_list = airdndsearchService.searchselect(param);
-		int size = search_list.size();
+			//if(filter_roomType_house)
 
-		List<JSONObject> homes = new ArrayList<JSONObject>();
+			List<AirdndSearchVO> search_list = airdndsearchService.searchselect(param);
+			int size = search_list.size();
 
-		Double[] maxmin_lat = new Double[2];
-		Double[] maxmin_lng = new Double[2];
+			List<JSONObject> homes = new ArrayList<JSONObject>();
 
-		for(int i = 0; i < size; i++) {
-			int home_idx = search_list.get(i).getHome_idx();
+			Double[] maxmin_lat = new Double[2];
+			Double[] maxmin_lng = new Double[2];
 
-			List<AirdndHomePictureVO> picture = airdndsearchService.pictureselect(home_idx);
-			List<String> picture_arr = new ArrayList<String>();
+			for(int i = 0; i < size; i++) {
+				int home_idx = search_list.get(i).getHome_idx();
 
-			JSONObject homes_info = new JSONObject();
-			JSONObject latlng = new JSONObject();
+				List<AirdndHomePictureVO> picture = airdndsearchService.pictureselect(home_idx);
+				List<String> picture_arr = new ArrayList<String>();
 
-			for(int j = 0; j < picture.size(); j++) {
-				picture_arr.add(picture.get(j).getUrl());
-			}
+				JSONObject homes_info = new JSONObject();
+				JSONObject latlng = new JSONObject();
 
-			search_list.get(i).setUrl(picture_arr);
-			double lat = Double.parseDouble(search_list.get(i).getLat());
-			double lng = Double.parseDouble(search_list.get(i).getLng());
-
-			for(int j = 0; j < homes.size(); j++) {
-				JSONObject lo = (JSONObject) homes.get(j).get("location");
-
-				if(lat == (Double)(lo.get("lat")) && lng == (Double)lo.get("lng")) {
-					lat += 0.01;
+				for(int j = 0; j < picture.size(); j++) {
+					picture_arr.add(picture.get(j).getUrl());
 				}
-			}
 
-			latlng.put("lat", lat);
-			latlng.put("lng", lng);
+				search_list.get(i).setUrl(picture_arr);
+				double lat = Double.parseDouble(search_list.get(i).getLat());
+				double lng = Double.parseDouble(search_list.get(i).getLng());
 
-			if(maxmin_lat[0] == null) {
-				maxmin_lat[0] = lat;
-			}else if(lat < maxmin_lat[0]) {
-				maxmin_lat[0] = lat;
-			}
+				for(int j = 0; j < homes.size(); j++) {
+					JSONObject lo = (JSONObject) homes.get(j).get("location");
 
-			if(maxmin_lat[1] == null) {
-				maxmin_lat[1] = lat;
-			}else if(lat > maxmin_lat[1]) {
-				maxmin_lat[1] = lat;
-			}
-
-			if(maxmin_lng[0] == null) {
-				maxmin_lng[0] = lng;
-			}else if(lng < maxmin_lng[0]) {
-				maxmin_lng[0] = lng;
-			}
-
-			if(maxmin_lng[1] == null) {
-				maxmin_lng[1] = lng;
-			}else if(lng > maxmin_lng[1]) {
-				maxmin_lng[1] = lng;
-			}
-
-			homes_info.put("homeId", search_list.get(i).getHome_idx());
-			homes_info.put("isSuperhost", search_list.get(i).getIsSuperHost());
-			homes_info.put("isBookmarked", "아직안받아옴");
-			homes_info.put("imageArray", search_list.get(i).getUrl());
-			homes_info.put("imageCount", search_list.get(i).getUrl().size());
-			homes_info.put("subTitle", search_list.get(i).getSub_title());
-			homes_info.put("title", search_list.get(i).getTitle());
-			homes_info.put("feature", "최대 인원 " + search_list.get(i).getFilter_max_person() + "명 · 침실 " + search_list.get(i).getFilter_bedroom() + 
-					"개 · 침대 " + search_list.get(i).getFilter_bed() + "개 · 욕실 " + search_list.get(i).getFilter_bathroom() + "개");
-			homes_info.put("rating", search_list.get(i).getRating());
-			homes_info.put("reviewCount", search_list.get(i).getReview_num());
-			homes_info.put("price", search_list.get(i).getPrice());
-			homes_info.put("location", latlng);
-
-			homes.add(homes_info);
-		}
-		Double avgLat =(Math.round((maxmin_lat[0] + maxmin_lat[1])/2*10000000)/10000000.0); 
-		Double avgLng =(Math.round((maxmin_lng[0] + maxmin_lng[1])/2*10000000)/10000000.0); 
-		//Double avgLng = (double) (Math.round((addLng/size)*10000000)/10000000);
-
-		res.put("homes", homes);
-
-		//가격 분포도--------------------------------
-		List<AirdndSearchVO> pricelist = airdndsearchService.unitpriceselect(location);
-		List<Integer> price_array = new ArrayList();
-		int start = 0;
-		int end = 2;
-		int save_num = 0;
-		for(int i = 0; i < 50; i++) {
-			for(AirdndSearchVO price : pricelist) {
-				if(start <= (int)price.getPrice()/10000 && end > (int)price.getPrice()/10000) {
-					save_num++;
-				}
-			}
-			price_array.add(save_num);
-			start += 2;
-			end += 2;
-			save_num = 0;
-		}
-
-		res.put("priceArray", price_array);
-
-		//필터조건들 ---------------------------------
-		List<AirdndSearchVO> facilities = airdndsearchService.facilityList(location);
-		//List<AirdndUserVO> hostlanlists = airdndsearchService.hostLanlist(location);
-
-		List<String> facilityListStr= new ArrayList<String>();
-		List<String> amenityListStr = new ArrayList<String>();
-		List<String> hostLangListStr = new ArrayList<String>();
-
-		for(AirdndSearchVO filter : facilities) {
-			if( !filter.getFacilityList().contains("이용 불가:") ) {
-				String element = filter.getFacilityList();
-				if( element.contains("수영장") ||  element.contains("주차") || element.contains("자쿠지") || element.contains("헬스장") ) {
-					facilityListStr.add(element);
-				} else {
-					amenityListStr.add(element);
-				}
-			}
-		}
-		hostLangListStr.add("영어");
-		hostLangListStr.add("프랑스어");
-		hostLangListStr.add("한국어");
-		hostLangListStr.add("스페인어");
-		hostLangListStr.add("중국어");
-
-		//for(AirdndUserVO filter : hostlanlists) {
-		//  hostlanguagelist.add(filter.getHostLanlist());
-		//}
-
-		JSONObject filterCondition = new JSONObject();
-		filterCondition.put("superhost", true);
-		filterCondition.put("amenityList", amenityListStr);
-		filterCondition.put("facilityList", facilityListStr);
-		filterCondition.put("hostLangList", hostLangListStr);
-
-		//--------최근 본 목록 코드 작성------------ 
-		List<JSONObject> recentHomes = new ArrayList<JSONObject>();//이걸 쿠키로 받아와 검색하는 쿼리문
-		List<Integer> recentHomesIdx = new ArrayList<Integer>();
-		List<AirdndSearchVO> recentHomeOne = new ArrayList<AirdndSearchVO>();
-
-		Cookie[] cookies = request.getCookies();
-
-		if(cookies == null) {
-		}else{
-			for (Cookie cookie : cookies) {
-				if(cookie.getName().contains("AirdndRH")) {
-					recentHomesIdx.add(Integer.parseInt(cookie.getName()));
-
-					//int recentHomesIdx[] = {596431, 4010129, 4165392};
-					for(int recenthome:recentHomesIdx) {
-						recentHomeOne = airdndsearchService.select_one(recenthome);
-
-						List<AirdndHomePictureVO> recentPicture = airdndsearchService.pictureselect(recenthome);
-
-						List<String> recent_picture_arr = new ArrayList<String>();
-
-						for(int j = 0; j < recentPicture.size(); j++) {
-							recent_picture_arr.add(recentPicture.get(j).getUrl());
-						}
-
-						JSONObject recentHome_info = new JSONObject();
-						JSONObject latlng = new JSONObject();
-
-						String lat = recentHomeOne.get(0).getLat();
-						String lng = recentHomeOne.get(0).getLng();
-						latlng.put("lat", lat);
-						latlng.put("lng", lng);
-
-						recentHomeOne.get(0).setUrl(recent_picture_arr);
-						recentHome_info.put("homeId", recentHomeOne.get(0).getHome_idx());
-						recentHome_info.put("isSuperhost", recentHomeOne.get(0).getIsSuperHost());
-						recentHome_info.put("isBookmarked", "아직안받아옴");
-						recentHome_info.put("imageArray", recentHomeOne.get(0).getUrl());
-						recentHome_info.put("imageCount", recentHomeOne.get(0).getUrl().size());
-						recentHome_info.put("subTitle", recentHomeOne.get(0).getSub_title());
-						recentHome_info.put("title", recentHomeOne.get(0).getTitle());
-						recentHome_info.put("rating", recentHomeOne.get(0).getRating());
-						recentHome_info.put("reviewCount", recentHomeOne.get(0).getReview_num());
-						recentHome_info.put("price", recentHomeOne.get(0).getPrice());
-						recentHome_info.put("location", latlng);
-
-						recentHomes.add(recentHome_info);
-
+					if(lat == (Double)(lo.get("lat")) && lng == (Double)lo.get("lng")) {
+						lat += 0.01;
 					}
-					//recentHomeList만 뿌려주면 끝
+				}
+
+				latlng.put("lat", lat);
+				latlng.put("lng", lng);
+
+				if(maxmin_lat[0] == null) {
+					maxmin_lat[0] = lat;
+				}else if(lat < maxmin_lat[0]) {
+					maxmin_lat[0] = lat;
+				}
+
+				if(maxmin_lat[1] == null) {
+					maxmin_lat[1] = lat;
+				}else if(lat > maxmin_lat[1]) {
+					maxmin_lat[1] = lat;
+				}
+
+				if(maxmin_lng[0] == null) {
+					maxmin_lng[0] = lng;
+				}else if(lng < maxmin_lng[0]) {
+					maxmin_lng[0] = lng;
+				}
+
+				if(maxmin_lng[1] == null) {
+					maxmin_lng[1] = lng;
+				}else if(lng > maxmin_lng[1]) {
+					maxmin_lng[1] = lng;
+				}
+
+				homes_info.put("homeId", search_list.get(i).getHome_idx());
+				homes_info.put("isSuperhost", search_list.get(i).getIsSuperHost());
+				homes_info.put("isBookmarked", "아직안받아옴");
+				homes_info.put("imageArray", search_list.get(i).getUrl());
+				homes_info.put("imageCount", search_list.get(i).getUrl().size());
+				homes_info.put("subTitle", search_list.get(i).getSub_title());
+				homes_info.put("title", search_list.get(i).getTitle());
+				homes_info.put("feature", "최대 인원 " + search_list.get(i).getFilter_max_person() + "명 · 침실 " + search_list.get(i).getFilter_bedroom() + 
+						"개 · 침대 " + search_list.get(i).getFilter_bed() + "개 · 욕실 " + search_list.get(i).getFilter_bathroom() + "개");
+				homes_info.put("rating", search_list.get(i).getRating());
+				homes_info.put("reviewCount", search_list.get(i).getReview_num());
+				homes_info.put("price", search_list.get(i).getPrice());
+				homes_info.put("location", latlng);
+
+				homes.add(homes_info);
+			}
+			Double avgLat =(Math.round((maxmin_lat[0] + maxmin_lat[1])/2*10000000)/10000000.0); 
+			Double avgLng =(Math.round((maxmin_lng[0] + maxmin_lng[1])/2*10000000)/10000000.0); 
+			//Double avgLng = (double) (Math.round((addLng/size)*10000000)/10000000);
+
+			res.put("homes", homes);
+
+			//가격 분포도--------------------------------
+			List<AirdndSearchVO> pricelist = airdndsearchService.unitpriceselect(location);
+			List<Integer> price_array = new ArrayList();
+			int start = 0;
+			int end = 2;
+			int save_num = 0;
+			for(int i = 0; i < 50; i++) {
+				for(AirdndSearchVO price : pricelist) {
+					if(start <= (int)price.getPrice()/10000 && end > (int)price.getPrice()/10000) {
+						save_num++;
+					}
+				}
+				price_array.add(save_num);
+				start += 2;
+				end += 2;
+				save_num = 0;
+			}
+
+			res.put("priceArray", price_array);
+
+			//필터조건들 ---------------------------------
+			List<AirdndSearchVO> facilities = airdndsearchService.facilityList(location);
+			//List<AirdndUserVO> hostlanlists = airdndsearchService.hostLanlist(location);
+
+			List<String> facilityListStr= new ArrayList<String>();
+			List<String> amenityListStr = new ArrayList<String>();
+			List<String> hostLangListStr = new ArrayList<String>();
+
+			for(AirdndSearchVO filter : facilities) {
+				if( !filter.getFacilityList().contains("이용 불가:") ) {
+					String element = filter.getFacilityList();
+					if( element.contains("수영장") ||  element.contains("주차") || element.contains("자쿠지") || element.contains("헬스장") ) {
+						facilityListStr.add(element);
+					} else {
+						amenityListStr.add(element);
+					}
 				}
 			}
+			hostLangListStr.add("영어");
+			hostLangListStr.add("프랑스어");
+			hostLangListStr.add("한국어");
+			hostLangListStr.add("스페인어");
+			hostLangListStr.add("중국어");
+
+			//for(AirdndUserVO filter : hostlanlists) {
+			//  hostlanguagelist.add(filter.getHostLanlist());
+			//}
+
+			JSONObject filterCondition = new JSONObject();
+			filterCondition.put("superhost", true);
+			filterCondition.put("amenityList", amenityListStr);
+			filterCondition.put("facilityList", facilityListStr);
+			filterCondition.put("hostLangList", hostLangListStr);
+
+			//--------최근 본 목록 코드 작성------------ 
+			List<JSONObject> recentHomes = new ArrayList<JSONObject>();//이걸 쿠키로 받아와 검색하는 쿼리문
+			List<Integer> recentHomesIdx = new ArrayList<Integer>();
+			List<AirdndSearchVO> recentHomeOne = new ArrayList<AirdndSearchVO>();
+
+			Cookie[] cookies = request.getCookies();
+
+			if(cookies == null) {
+			}else{
+				for (Cookie cookie : cookies) {
+					if(cookie.getName().contains("AirdndRH")) {
+						recentHomesIdx.add(Integer.parseInt(cookie.getName()));
+
+						//int recentHomesIdx[] = {596431, 4010129, 4165392};
+						for(int recenthome:recentHomesIdx) {
+							recentHomeOne = airdndsearchService.select_one(recenthome);
+
+							List<AirdndHomePictureVO> recentPicture = airdndsearchService.pictureselect(recenthome);
+
+							List<String> recent_picture_arr = new ArrayList<String>();
+
+							for(int j = 0; j < recentPicture.size(); j++) {
+								recent_picture_arr.add(recentPicture.get(j).getUrl());
+							}
+
+							JSONObject recentHome_info = new JSONObject();
+							JSONObject latlng = new JSONObject();
+
+							String lat = recentHomeOne.get(0).getLat();
+							String lng = recentHomeOne.get(0).getLng();
+							latlng.put("lat", lat);
+							latlng.put("lng", lng);
+
+							recentHomeOne.get(0).setUrl(recent_picture_arr);
+							recentHome_info.put("homeId", recentHomeOne.get(0).getHome_idx());
+							recentHome_info.put("isSuperhost", recentHomeOne.get(0).getIsSuperHost());
+							recentHome_info.put("isBookmarked", "아직안받아옴");
+							recentHome_info.put("imageArray", recentHomeOne.get(0).getUrl());
+							recentHome_info.put("imageCount", recentHomeOne.get(0).getUrl().size());
+							recentHome_info.put("subTitle", recentHomeOne.get(0).getSub_title());
+							recentHome_info.put("title", recentHomeOne.get(0).getTitle());
+							recentHome_info.put("rating", recentHomeOne.get(0).getRating());
+							recentHome_info.put("reviewCount", recentHomeOne.get(0).getReview_num());
+							recentHome_info.put("price", recentHomeOne.get(0).getPrice());
+							recentHome_info.put("location", latlng);
+
+							recentHomes.add(recentHome_info);
+
+						}
+						//recentHomeList만 뿌려주면 끝
+					}
+				}
+			}
+
+			JSONObject mapCenter = new JSONObject();
+			mapCenter.put("lat", avgLat);
+			mapCenter.put("lng", avgLng);
+
+			res.put("filterCondition", filterCondition);
+			//전체 숙소 데이터 개수, 1박 평균 가격 -----------------
+			List<AirdndSearchVO> total = airdndsearchService.searchtotalselect(param);
+
+			res.put("recentHomes", recentHomes);
+			res.put("dataTotal", total.get(0).getData_total());
+			res.put("averagePrice", total.get(0).getAverage_price());
+			res.put("mapCenter",mapCenter);
 		}
-
-		JSONObject mapCenter = new JSONObject();
-		mapCenter.put("lat", avgLat);
-		mapCenter.put("lng", avgLng);
-
-		res.put("filterCondition", filterCondition);
-		//전체 숙소 데이터 개수, 1박 평균 가격 -----------------
-		List<AirdndSearchVO> total = airdndsearchService.searchtotalselect(param);
-
-		res.put("recentHomes", recentHomes);
-		res.put("dataTotal", total.get(0).getData_total());
-		res.put("averagePrice", total.get(0).getAverage_price());
-		res.put("mapCenter",mapCenter);
 
 		return res.toString();
 	}
